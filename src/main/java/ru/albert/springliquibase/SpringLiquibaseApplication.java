6dd1aee6bc460970c54dc9a985615257356fac73
Albert Nigmatullin <albertnig16@gmail.com>
2024-06-30 11:01:43 +0500

    Test commit

--- src/main/java/ru/albert/springliquibase/SpringLiquibaseApplication.java
+++ src/main/java/ru/albert/springliquibase/SpringLiquibaseApplication.java
@@ -7,4 +7,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringLiquibaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLiquibaseApplication.class, args);	}}
+		SpringApplication.run(SpringLiquibaseApplication.class, args);	  }}

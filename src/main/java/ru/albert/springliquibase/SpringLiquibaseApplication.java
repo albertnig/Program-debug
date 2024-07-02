85137f76686deb78451a520b7145eaa30b80ffe3
Albert Nigmatullin <albertnig16@gmail.com>
2024-07-02 13:10:38 +0500

    I wait error

--- src/main/java/ru/albert/springliquibase/SpringLiquibaseApplication.java
+++ src/main/java/ru/albert/springliquibase/SpringLiquibaseApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringLiquibaseApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLiquibaseApplication.class, args);
-	}}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringLiquibaseApplication.class, args);
+  }
+}

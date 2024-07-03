9e0f47680f45d34ed1aa32677dcf633e2b0d570f
Albert Nigmatullin <albertnig16@gmail.com>
2024-07-03 15:32:57 +0500

    Test pre-commit hook

--- src/main/java/ru/albert/springliquibase/SpringLiquibaseApplication.java
+++ src/main/java/ru/albert/springliquibase/SpringLiquibaseApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringLiquibaseApplication {
   public static void main(String[] args) {
     SpringApplication.run(SpringLiquibaseApplication.class, args);
-        }
+  }
 }
+// some comment
 // some comment
 // some comment
